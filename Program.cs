d527c679a6602774281506ffd68d3d70ce1af813
M S I <M S I@MSI>
2022-10-24 16:21:49 +0700

    add while and console writline

--- Program.cs
+++ Program.cs
@@ -14,6 +14,21 @@ namespace SearchAlgorithem
         int n;
         int i;
 
+        public void input()
+        {
+            while (true)
+            {
+                Console.WriteLine("eter the number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20 ))
+                    break;
+                else
+                    Console.WriteLine("\nArray shaould have minumun 1 and maximunm 20 elements.\n");
+            }
+        }
+        
+            
 
         static void Main(string[] args)
         {
