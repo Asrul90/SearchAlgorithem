d2155a71e8397217bcab78ea11c985e9b6fff74e
M S I <M S I@MSI>
2022-10-24 16:52:55 +0700

    apply binary and obtain the index

--- Program.cs
+++ Program.cs
@@ -38,6 +38,40 @@ namespace SearchAlgorithem
                 arr[i] = Int32.Parse(s1);
             }
         }
+
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                //accept the number to be searhed
+                Console.Write("\nEnter elements wants you to search :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                //apply binary search
+                int lowerbound = 0;
+                int upperbound = 0;
+
+                //obtain the index of the midle elements
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                // loop to search for the elements in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid = 1;
+                    else
+                        upperbound = mid - 1;
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item > arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + " found at position " + (mid + 1).ToString());
+                else 
+
+            }
+        }
         
             
 
